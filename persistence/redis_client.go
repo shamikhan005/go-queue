dde2f58617a2126c2664eccf2891f5c01114c86a
shamikhan005 <shamikhan8595@gmail.com>
2025-01-19 20:52:13 +0530

    add Redis client implementation for task queue management

---
+++ persistence/redis_client.go
@@ -0,0 +1,31 @@
+package persistence
+
+import (
+	"context"
+	"log"
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+type RedisClient struct {
+	Client *redis.Client
+}
+
+func NewRedisClient() *RedisClient {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	if err := client.Ping(ctx).Err(); err != nil {
+		log.Fatalf("failed to connect to redis: %v", err)
+	}
+	return &RedisClient{Client: client}
+}
+
+func (r *RedisClient) Addtask(queue string, taskData string) error {
+	return r.Client.RPush(ctx, queue, taskData).Err()
+}
+
+func (r *RedisClient) GetTask(queue string) (string, error) {
+	return r.Client.LPop(ctx, queue).Result()
+}
